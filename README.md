c18f52c3ebdcb704167cefdf1763f6def4dbab5d
kainlite <kainlite@gmail.com>
2019-05-01 02:32:11 -0300

    Added build status to the readme
    Rename travis file
    
    Added log and gitignore
    
    Parse port
    
    Fake test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Example repo
+
+[![Build Status](https://travis-ci.org/kainlite/whatismyip-go.svg?branch=master)](https://travis-ci.org/kainlite/whatismyip-go)
--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func httphandler(w http.ResponseWriter, r *http.Request) {
 	ipAddress, _, _ := net.SplitHostPort(r.RemoteAddr)
 	fmt.Fprintf(w, "%s", ipAddress)
 }
 
 func main() {
-	listener := fmt.Sprintf(":%d", os.Setenv("WHATISMYIP_PORT", "8000"))
+	port, err := strconv.Atoi(os.Getenv("WHATISMYIP_PORT"))
+	if err != nil {
+		log.Fatalf("Please make sure the environment variable WHATISMYIP_PORT is defined and is a valid integer [1024-65535], error: %s", err)
+	}
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(listener, nil)
+	listener := fmt.Sprintf(":%d", port)
+
+	http.HandleFunc("/", httphandler)
+	log.Fatal(http.ListenAndServe(listener, nil))
 }
