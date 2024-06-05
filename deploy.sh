f9958209ca0c0788ef51a007d031f52929d85cdf
Raphaël Pinson <raphael@isovalent.com>
2024-06-05 13:57:30 +0200

    Pass GCP_PROJECCT as it's not automatic anymore
    Signed-off-by: Raphaël Pinson <raphael@isovalent.com>

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,5 @@ gcloud functions deploy "${FUNCTION}" \
   --project=cilium-demo \
   --gen2 --runtime=go122 --region=europe-west1 --source=. \
   --entry-point="$ENTRYPOINT" --trigger-http --allow-unauthenticated \
+  --set-env-vars GCP_PROJECT=cilium-demo \
   --set-secrets="$SECRETS"
